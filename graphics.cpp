d20a4319433ab45780a657aff3593d7e8c17d5b1
kamieen03 <kamieen03@gmail.com>
2021-12-04 16:39:34 +0100

    Add external graphics module

---
+++ graphics.cpp
@@ -0,0 +1,59 @@
+#include "graphics.hpp"
+
+namespace
+{
+    cv::Size getTextSize(const std::string& text, float fontScale)
+    {
+        int baseline {0};
+        return cv::getTextSize(text,
+                               graphics::FONT_FACE,
+                               fontScale,
+                               graphics::FONT_THICKNESS,
+                               &baseline);
+    }
+}
+
+namespace graphics
+{
+    cv::Mat getColoredRectangleWithCenteredText(int height,
+                                                int width,
+                                                cv::Scalar color,
+                                                const std::string& text,
+                                                float fontScale)
+    {
+        cv::Mat img{height, width, CV_8UC3, color};
+        const auto size = getTextSize(text, fontScale);
+        const auto pos = cv::Point((width - size.width) / 2, (height + size.height) / 2);
+        cv::putText(img, text, pos, FONT_FACE, fontScale, FONT_COLOR, FONT_THICKNESS * fontScale);
+        return img;
+    }
+
+    void addBorder(cv::Mat& image, cv::Scalar borderColor)
+    {
+        cv::rectangle(image,
+                      cv::Point(0,0),
+                      cv::Point(image.cols, image.rows),
+                      borderColor,
+                      BORDER_THICKNESS); //thickness
+    }
+
+
+    void pasteRectangleOntoImage(const cv::Mat& rectangle,
+                                 cv::Mat& image,
+                                 const cv::Point& origin)
+    {
+        const cv::Size size(rectangle.cols, rectangle.rows);
+        auto dst = image(cv::Rect(origin, size));
+        rectangle.copyTo(dst);
+    }
+
+    void pasteRectangleOntoImageCentered(const cv::Mat& rectangle,
+                                         cv::Mat& image)
+    {
+        const auto w = (image.cols - rectangle.cols) / 2;
+        const auto h = (image.rows - rectangle.rows) / 2;
+        const cv::Point origin(w, h);
+        pasteRectangleOntoImage(rectangle, image, origin);
+    }
+}
+
---
+++ graphics.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+
+namespace graphics
+{
+    constexpr int    FONT_FACE {cv::FONT_HERSHEY_DUPLEX};
+    constexpr int    FONT_THICKNESS {2};
+    const cv::Scalar FONT_COLOR = CV_RGB(0,0,0);
+    const cv::Scalar BORDER_COLOR = CV_RGB(200,100,20);
+    constexpr int BORDER_THICKNESS {10};
+
+    cv::Mat getColoredRectangleWithCenteredText(int height,
+                                                int width,
+                                                cv::Scalar color,
+                                                const std::string& text,
+                                                float fontScale = 1.0);
+    void addBorder(cv::Mat& image, cv::Scalar borderColor = BORDER_COLOR);
+    void pasteRectangleOntoImage(const cv::Mat& rectangle,
+                                 cv::Mat& image,
+                                 const cv::Point& origin);
+    void pasteRectangleOntoImageCentered(const cv::Mat& rectangle,
+                                         cv::Mat& image);
+}
+
