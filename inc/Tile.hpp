aa2a4fbcab6799a22b730e6ed90c3e8f6613acd4
kamieen03 <kamieen03@gmail.com>
2021-12-05 16:29:03 +0100

    Refactor Tile

--- inc/Tile.hpp
+++ inc/Tile.hpp
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "ColorSchemeReader.hpp"
+#include "graphics.hpp"
 
 using Coordinate = std::pair<int,int>;
 
@@ -15,26 +16,22 @@ public:
     Tile(){};
     Tile(Coordinate c);
     void updateTile(int value);
+    void updateColorScheme(const ColorScheme& cs);
+
     void doubleTile(){updateTile(value*2);};
     cv::Mat getFace() {return face;};
     int getValue() const {return value;};
     bool empty() const {return value == 0;};
-    void updateColorScheme(const ColorScheme& cs);
     Coordinate getPosition() const {return position;}
 
     static constexpr int TILE_SIZE {150};
 
 private:
-    void setFaceColor(int value);
-    void setText(int val);
+    cv::Scalar getColor(int value);
 
-    static constexpr int FONT_FACE {cv::FONT_HERSHEY_DUPLEX};
-    static constexpr float FONT_SCALE {2};
-    static constexpr int FONT_THICKNESS {2};
-    static const cv::Scalar FONT_COLOR;
     static const ColorScheme* colorScheme;
-
     int value {0};
     Coordinate position;
     cv::Mat face {TILE_SIZE, TILE_SIZE, CV_8UC3, CV_RGB(0,0,0)};
 };
+
--- src/Tile.cpp
+++ src/Tile.cpp
@@ -1,7 +1,6 @@
 #include "Tile.hpp"
 #include <iostream>
 
-const cv::Scalar Tile::FONT_COLOR = CV_RGB(0,0,0);
 const ColorScheme* Tile::colorScheme {nullptr};
 
 Tile::Tile(Coordinate c):
@@ -10,46 +9,25 @@ Tile::Tile(Coordinate c):
 
 void Tile::updateTile(int val)
 {
-
     value = val;
-    setFaceColor(value);
-    if(value != 0)
-    {
-        setText(value);
-    }
+    const auto color = getColor(value);
+    const auto text = (value == 0) ? "" : std::to_string(value);
+    face = graphics::getColoredRectangleWithCenteredText(
+            TILE_SIZE, TILE_SIZE, color, text, 2);
 }
 
-void Tile::setText(int val)
+cv::Scalar Tile::getColor(int val)
 {
-    int baseline;
-    cv::Size s = cv::getTextSize(std::to_string(val),
-                                 FONT_FACE,
-                                 FONT_SCALE,
-                                 FONT_THICKNESS,
-                                 &baseline);
-    cv::putText(face,
-                std::to_string(val),
-                cv::Point((TILE_SIZE - s.width) / 2, (TILE_SIZE + s.height) / 2), // bottom-left corner
-                FONT_FACE,
-                FONT_SCALE,
-                FONT_COLOR,
-                FONT_THICKNESS);
-}
-
-void Tile::setFaceColor(int val)
-{
-    cv::Scalar color;
     if(val == 0)
     {
-        color = colorScheme->getEmptyTileColor();
+        return colorScheme->getEmptyTileColor();
     }
     else
     {
         const auto& colorMap = colorScheme->getTileColors();
         const auto it = colorMap.find(val);
-        color = (it != colorMap.end()) ? it->second : colorMap.at(2048);
+        return (it != colorMap.end()) ? it->second : colorMap.at(2048);
     }
-    face = cv::Mat(TILE_SIZE, TILE_SIZE, CV_8UC3, color);
 }
 
 void Tile::updateColorScheme(const ColorScheme& cs)
@@ -57,3 +35,4 @@ void Tile::updateColorScheme(const ColorScheme& cs)
     colorScheme = &cs;
     updateTile(value);
 }
+
